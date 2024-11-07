05ccd00d2c824adc415f9906b8452f8d81c3df73
sadia-pao <aandiyumbo@gmail.com>
2024-11-07 15:00:44 -0500

    come

--- src/main/java/com/mycompany/anahiandi2026/AnahiAndi2026.java
+++ src/main/java/com/mycompany/anahiandi2026/AnahiAndi2026.java
@@ -1,16 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.anahiandi2026;
 
-/**
- *
- * @author SO-LAB1-PC21
- */
+import java.util.Scanner;
+
 public class AnahiAndi2026 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+
+        float[] notas = new float[5];
+        float promedio = 0;
+        float notaAlta, notaBaja;
+        int aprobados = 0, reprobados = 0;
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("ingrese la nota del estudiante ");
+            notas[i] = sc.nextFloat();
+        }
+        System.out.println("INGRESE UNA OPCION DEL 1 AL 5");
+        System.out.println("CALCULAR EL PROMEDIO DE NOTAS");
+        System.out.println("MOSTRAR LA NOTA MAS ALTA Y A MAS BAJA");
+        System.out.println("CONTAR APROBADOS Y EPROBADOS");
+        System.out.println("SALIR--");
+        
+        int menu =sc.nextInt();
+        switch (menu){
+            case 1:
+                float suma=0;
+                for (int i = 0;i < 5; i++){
+                    suma''
+                
+            }
+                
+                
+        }
+        
+    }    
+            
+}    
